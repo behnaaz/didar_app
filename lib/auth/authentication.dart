1a015da7b8df4289df0bfded51ae33b82d5eec11
Behnaz <behnaz.changizi@gmail.com>
2021-08-04 01:26:01 +0100

    Make authentication.dart compilable, application_status is not yet fixed

--- lib/auth/authentication.dart
+++ lib/auth/authentication.dart
@@ -33,47 +33,52 @@ class Auth implements BaseAuth {
     	}
     }
 
-    return _firebaseAuth.currentUser.uid;
+    return Future<String>.value(_firebaseAuth?.currentUser?.uid);
   }
 
   Future<String> signUp(final String email, final String password) async {
-    try {
+   try {
       await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
-    } on FirebaseAuthException catch (e) {
-    if (e.code == 'weak-password') {
+     } on FirebaseAuthException catch (e) {
+     if (e.code == 'weak-password') {
         throw Exception('The password provided is too weak.');
-    } if (e.code == 'email-already-in-use') {
+     } if (e.code == 'email-already-in-use') {
     	throw Exception('The account already exists for ' + email);
-    }
+     }
+   }
 
-    return _firebaseAuth.currentUser.uid;
+    return Future<String>.value(_firebaseAuth?.currentUser?.uid);
   }
 
 
   Future<User> getCurrentUser() async {
-    return _firebaseAuth.currentUser;
+    final User? user = await _firebaseAuth.currentUser;
+    return Future<User>.value(user);
   }
 
 
   Future<void> signOut() async {
-    return _firebaseAuth.signOut();
+    return await _firebaseAuth.signOut();
   }
   
   
 // TODO:
 //  replace the commented code with https://firebase.flutter.dev/docs/auth/usage/
-/*
+// To import 'package:didar_app/auth/authentication.dart';
   Future<void> sendEmailVerification() async {
-    User user = await _firebaseAuth.currentUser();
-    user.sendEmailVerification();
+       throw Exception('Not implemented yet');
+//    User user = await _firebaseAuth.currentUser();
+//    user.sendEmailVerification();
   }
 
   Future<bool> isEmailVerified() async {
-    User user = await _firebaseAuth.currentUser();
-    return user.isEmailVerified;
+       throw Exception('Not implemented yet');
+    //User user = await _firebaseAuth.currentUser();
+    //return user.isEmailVerified;
   }
 
   Future<void> sendPasswordReset(String email) async {
-    _firebaseAuth.sendPasswordResetEmail(email: email);
-  }*/
+       throw Exception('Not implemented yet');
+    //_firebaseAuth.sendPasswordResetEmail(email: email);
+  }
 }
