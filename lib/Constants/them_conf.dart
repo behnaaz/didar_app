11dce1a70a6ba028cc825012075f1ca8dd2b031e
sajjad Alizadeh <samycrip10@gmail.com>
2021-08-05 11:41:34 +0430

    Add them configuration constats

---
+++ lib/Constants/them_conf.dart
@@ -0,0 +1,7 @@
+// ::> Colors
+import 'package:flutter/cupertino.dart';
+
+const Color kBlue = Color(0xff00b2e2);
+const Color kRed = Color(0xfff24137);
+const Color kGrayBg = Color(0xfff4f4f4);
+const Color kTextColor = Color(0xff606060);
