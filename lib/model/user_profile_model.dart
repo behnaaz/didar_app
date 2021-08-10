16af909597d15a829c875e4335b30c6127b56912
sajjad Alizadeh <samycrip10@gmail.com>
2021-08-10 23:24:39 +0430

    update user profile model

---
+++ lib/model/user_profile_model.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/foundation.dart';
+
+@immutable
+class UserProfile {
+  final String fullName;
+  final String email;
+  final int phoneNumber;
+  final int age;
+
+  UserProfile({
+    //TODO : Profile property shulde be complited
+    required this.fullName,
+    required this.email,
+    required this.phoneNumber,
+    required this.age,
+  });
+
+  factory UserProfile.fromJson(final json) {
+    return UserProfile(
+      fullName: json["name"],
+      email: json["id"],
+      phoneNumber: json["symbol"],
+      age: json["currency"],
+    );
+  }
+}
