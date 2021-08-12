ef344893db809a450ed6af0d3814b179afb55e01
sajjad Alizadeh <samycrip10@gmail.com>
2021-08-12 16:56:06 +0430

    update register screen

--- lib/screen/register_screen.dart
+++ lib/screen/register_screen.dart
@@ -1,12 +1,8 @@
 import 'package:didar_app/auth/authenticatService.dart';
-import 'package:didar_app/screen/login_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:provider/provider.dart';
-import 'home_screen.dart';
 
 class RegisterScreen extends StatefulWidget {
-  // TextField Controllers
   @override
   State<RegisterScreen> createState() => _RegisterScreenState();
 }
@@ -18,131 +14,140 @@ class _RegisterScreenState extends State<RegisterScreen> {
   final TextEditingController rePasswordController = TextEditingController();
   final TextEditingController fullNameController = TextEditingController();
 
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void createAccount(authService) async {
+    if (passwordController.text == rePasswordController.text) {
+      await authService.signUp(
+          fullName: fullNameController.text,
+          email: emailController.text,
+          password: passwordController.text);
+      print("i am registered succecfully"); // LOG : user registered
+      Navigator.pop(context);
+    }
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     final authService = Provider.of<AuthenticationService>(context);
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: ListView(
-          children: [
-            SizedBox(
-              height: 180,
-            ),
-            Text(
-              'Register',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              SizedBox(
+                height: 180,
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5),
-              child: TextField(
-                controller: fullNameController,
-                decoration: InputDecoration(
-                  labelText: "Full name",
-                  prefixIcon: Icon(Icons.person),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
+              Text(
+                'Register',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: TextField(
+                  controller: fullNameController,
+                  decoration: InputDecoration(
+                    labelText: "Full name",
+                    prefixIcon: Icon(Icons.person),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5),
-              child: TextField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  labelText: "Email",
-                  prefixIcon: Icon(Icons.email),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    prefixIcon: Icon(Icons.email),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5),
-              child: TextField(
-                controller: passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: "Password",
-                  prefixIcon: Icon(Icons.lock),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    prefixIcon: Icon(Icons.lock),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5),
-              child: TextField(
-                controller: rePasswordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: "Password confirm",
-                  prefixIcon: Icon(Icons.lock),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: TextField(
+                  controller: rePasswordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Password confirm",
+                    prefixIcon: Icon(Icons.lock),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
                   ),
                 ),
               ),
-            ),
-            passwordController.text == rePasswordController.text
-                ? Text('')
-                : Text('Password Confirm are not match!',style:TextStyle(color: Colors.redAccent[700]) ,),
-            SizedBox(
-              height: 30,
-            ),
-            ElevatedButton(
-              style: ButtonStyle(
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+              passwordController.text == rePasswordController.text
+                  ? Text('')
+                  : Text(
+                      'Password Confirm are not match!',
+                      style: TextStyle(color: Colors.redAccent[700]),
+                    ),
+              SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
                   ),
                 ),
-              ),
-              onPressed: () async {
-                if(passwordController.text == rePasswordController.text){
-
-                await authService.signUp(
-                    fullName: fullNameController.text,
-                    email: emailController.text,
-                    password: passwordController.text);
-                print("i am registered succecfully"); // LOG : user registered
-                Navigator.pop(context);
-                }
-                setState(() {
-                  
-                });
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Text("Create account"),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Have an account"),
-                SizedBox(
-                  width: 5,
+                onPressed: () {
+                  createAccount(authService);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Text("Create account"),
                 ),
-                OutlinedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text("Login"),
-                )
-              ],
-            )
-          ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Have an account"),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  OutlinedButton(
+                    child: Text("Login"),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
