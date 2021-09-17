9046a55f32bf619304b976808fc64810a32ebbcf
sajjad Alizadeh <samycrip10@gmail.com>
2021-09-17 14:41:35 +0430

    :truck: move clock time to SolarCalendar

--- lib/screen/_bottom_nav_wrapper.dart
+++ lib/screen/_bottom_nav_wrapper.dart
@@ -20,7 +20,7 @@ class BottomNavigationWrapper extends StatefulWidget {
 }
 
 class _BottomNavigationWrapperState extends State<BottomNavigationWrapper> {
-  int _selectedIndex = 1;
+  int _selectedIndex = 3;
 
   // NOTE : Bottom navigation item widgetOptions
   final List<Widget> _widgetOptions = <Widget>[
--- lib/screen/calendar_weekly_screen.dart
+++ lib/screen/calendar_weekly_screen.dart
@@ -125,7 +125,7 @@ class CalendarWeeklyScreen extends StatelessWidget {
                                       child: Container(
                                           color: ColorPallet.lightGrayBg,
                                           child: index < 23
-                                              ? Text((_clockTime[index + 1]))
+                                              ? Text((SolarCalendar.clockTime[index + 1]))
                                               : null)),
                                   Row(
                                     children: [
@@ -141,9 +141,9 @@ class CalendarWeeklyScreen extends StatelessWidget {
                                                 SolarCalendar.daysOfTheWeek[i] +
                                                     ' | ' +
                                                     'from ' +
-                                                    _clockTime[index] +
+                                                    SolarCalendar.clockTime[index] +
                                                     ' to ' +
-                                                    _clockTime[index + 1],
+                                                    SolarCalendar.clockTime[index + 1],
                                               );
                                             },
                                             child: Container(
@@ -174,31 +174,3 @@ class CalendarWeeklyScreen extends StatelessWidget {
   }
 }
 
-// REMOVE later
-List<String> _clockTime = [
-  '00:00',
-  '01:00',
-  '02:00',
-  '03:00',
-  '04:00',
-  '05:00',
-  '06:00',
-  '07:00',
-  '08:00',
-  '09:00',
-  '10:00',
-  '11:00',
-  '12:00',
-  '13:00',
-  '14:00',
-  '15:00',
-  '16:00',
-  '17:00',
-  '18:00',
-  '19:00',
-  '20:00',
-  '21:00',
-  '22:00',
-  '23:00',
-  '00:00',
-];
