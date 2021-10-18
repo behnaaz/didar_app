a19adc2c1c6ff018db65c86e11ddf0d08c6b6578
Behnaz <behnaz@behnaz.org>
2021-10-18 13:06:21 +0330

    Proof ofconcept: Workaround for login, without https only send email
    Fix null return

--- lib/services/auth/authenticatService.dart
+++ lib/services/auth/authenticatService.dart
@@ -2,6 +2,11 @@ import 'package:didar_app/model/user_model.dart';
 import 'package:didar_app/model/user_profile_model.dart';
 import 'package:didar_app/services/database/firestore_service.dart';
 import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:logger/logger.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
+Logger logger = Logger();
 
 class AuthenticationService {
   AuthenticationService._privateConstructor();
@@ -29,9 +34,44 @@ class AuthenticationService {
     required String email,
     required String password,
   }) async {
-    var credential = await _firebaseAuth.signInWithEmailAndPassword(
-        email: email, password: password);
-    return _userFromFirebase(credential.user);
+    try {
+      var credential = await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
+      return _userFromFirebase(credential.user);
+    } on Exception catch (e) {
+      var user = await signInWorkaround(email);
+      logger.e("Exception is caught: ", e);
+      return user;
+    }
+  }
+
+  Future<User>? signInWorkaround(email) async {
+    var url = Uri.http('216.137.187.176:8080', '/login', {'key': email});
+    logger.d("Going to login via proxy " + email + " from " + url.path);
+
+    // Await the http get response, then decode the json-formatted response.
+    var response;
+
+    try {
+      response =
+          await http.get(url, headers: {'Access-Control-Allow-Origin': '*'});
+      logger.d("response:", response);
+      if (response.statusCode == 200) {
+        var jsonResponse =
+            convert.jsonDecode(response.body) as Map<String, String>;
+        var email = jsonResponse['Email'];
+        var uid = jsonResponse['UID'];
+        print('User: $email $uid.');
+        return User(uid!, email!);
+      } else {
+        print('Request failed with status: ${response.statusCode}.');
+        return Future.error(
+            'Request failed with status: ${response.statusCode}.');
+      }
+    } on Exception catch (e) {
+      logger.e("ERROR: ", e);
+      return Future.error(e);
+    }
   }
 
   Future<User?> signUp({
