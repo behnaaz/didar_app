2818d9f10c032c892d8e52c3e039da2a2011718f
Behnaz <behnaz@behnaz.org>
2021-10-31 23:42:27 +0330

    Encapsulate the use of firestore in firestore_service

--- lib/services/database/firestore_service.dart
+++ lib/services/database/firestore_service.dart
@@ -1,24 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:didar_app/model/user_profile_model.dart';
-import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:didar_app/services/auth/authenticatService.dart';
 
 class FirestoreServiceDB {
-  
-
-  final auth.FirebaseAuth _firebaseAuth = auth.FirebaseAuth.instance;
+  final AuthenticationService _authService;
+  FirestoreServiceDB(this._authService);
+  String get uid => _authService.currentUser!.uid;
 
   final CollectionReference _userProfilesCollection =
       FirebaseFirestore.instance.collection('user_profile');
 
   /// initial user profile data >> I use it in register >> AuthenticationService.signUp
   Future addUserProfileData(userData) async {
-    String uid = _firebaseAuth.currentUser!.uid;
     return await _userProfilesCollection.doc(uid).set(userData);
   }
 
   /// Update UserDate
   Future updateUserData(userData) async {
-    String uid = _firebaseAuth.currentUser!.uid;
     return await _userProfilesCollection
         .doc(uid)
         .set(userData, SetOptions(merge: true));
@@ -26,7 +24,6 @@ class FirestoreServiceDB {
 
   /// Update Session_topic
   Future updateSessionTopic(List<String> sessionList) async {
-    String uid = _firebaseAuth.currentUser!.uid;
     return await _userProfilesCollection
         .doc(uid)
         .set({'session_topics': sessionList}, SetOptions(merge: true));
@@ -35,7 +32,6 @@ class FirestoreServiceDB {
   /// add new social links
   Future addNewSocialLink(
       String label, String link, List<dynamic> socialList) async {
-    String uid = _firebaseAuth.currentUser!.uid;
     bool edit = false;
     for (var i = 0; i < socialList.length; i++) {
       if (socialList[i].containsKey(label)) {
@@ -53,10 +49,7 @@ class FirestoreServiceDB {
   }
 
   Future<UserProfile> get userProfile {
-    return _userProfilesCollection
-        .doc(_firebaseAuth.currentUser!.uid)
-        .get()
-        .then((value) => UserProfile.fromJson(
-            value.data())); //TODO if null TODO Options for get
+    return _userProfilesCollection.doc(uid).get().then((value) =>
+        UserProfile.fromJson(value.data())); //TODO if null TODO Options for get
   }
 }
